c5e5610b0e8850ee9a934611897fe0ae10ca7136
dlandy1 <dlandy1@babson.edu>
2015-10-22 15:59:42 -0700

    cat

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   $("#scratch").click(function(){
     
-    var text = $("#cat").val().allReplace({'ver': 'vepurr', 'fer': 'fur', 'for': 'fur', ' for': ' fur', 'full': 'furll', 'For': 'Fur', 'Hi': 'Meow!😸', 'Yo': 'Meow!😸', 'Hello': 'Meow!😸','Hey': 'Meow!😸', ' arr': ' purr', 'awesome': 'meow-velous', 'great': 'as great as catnip', 'good': 'meow-velous','er ': 'epurr ', 'ers ': 'epurrs ', 'were': 'wrrr', 'ber ': 'purr ', 'ber': 'purr', 'per' : 'purr', '%' : ' purr-cent', 'thanks': 'back scratches', 'thank': 'back scratch', 'from': 'furom', 'From': 'Furom', 'feeling': 'feline', 'Product': 'Purr-oduct', 'product': 'purr-oduct', 'mou': 'meow', 'br' : 'purr-', 'br ' : 'purr ', 'team': 'litter', 'followers': 'litter', 'people': 'cats', 'wrrr' : 'were', 'pir' : 'purr', 'kidding' : 'kitten', 'say' : 'meow', 'saying' : 'meowing', 'said' : 'meowed' });
+    var text = $("#cat").val().allReplace({'ver': 'vepurr', 'fer': 'fur', 'for': 'fur', ' for': ' fur', 'full': 'furll', 'For': 'Fur', 'Hi': 'Meow!😸', 'Yo': 'Meow!😸', 'Hello': 'Meow!😸','Hey': 'Meow!😸', ' arr': ' purr', 'awesome': 'meow-velous', 'great': 'as great as catnip', 'good': 'meow-velous','er ': 'epurr ', 'ers ': 'epurrs ', 'were': 'wrrr', 'ber ': 'purr ', 'ber': 'purr', 'per' : 'purr', '%' : ' purr-cent', 'thanks': 'back scratches', 'thank': 'back scratch', 'from': 'furom', 'From': 'Furom', 'feeling': 'feline', 'Product': 'Purr-oduct', 'product': 'purr-oduct', 'mou': 'meow', 'br' : 'purr-', 'br ' : 'purr ', 'team': 'litter', 'followers': 'litter', 'people': 'cats', 'wrrr' : 'were', 'pir' : 'purr', 'kidding' : 'kitten', 'say' : 'meow', 'saying' : 'meowing', 'said' : 'meowed', 'community' : 'cat park' });
     $(".vertical").html(text);
   })
 
