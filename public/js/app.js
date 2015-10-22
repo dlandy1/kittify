bc8df534659d4c48c7d1c28cf91ef4429ab56808
dlandy1 <dlandy1@babson.edu>
2015-10-22 15:18:40 -0700

    Initial static site template app

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  String.prototype.allReplace = function(obj) {
+    var retStr = this;
+    for (var x in obj) {
+        retStr = retStr.replace(new RegExp(x, 'g'), obj[x]);
+    }
+    return retStr;
+};
+
+  $("#scratch").click(function(){
+    var text = $("#cat").val().allReplace({'ver': 'vepurr', 'fer': 'fur', 'for': 'fur', ' for': ' fur', 'full': 'furll', 'For': 'Fur', 'Hi': 'Meow!😸', 'Hello': 'Meow!😸','Hey': 'Meow!😸', ' arr': ' purr', 'awesome': 'meow-velous', 'great': 'as great as catnip', 'good': 'meow-velous','er ': 'epurr ', 'ers ': 'epurrs ', 'were': 'wrrr', 'er. ': 'epurr. ', 'ber ': 'purr ', 'ber': 'purr', 'per' : 'purr', '%' : ' purr-cent', 'thanks': 'back scratches', 'thank': 'back scratch', 'from': 'furom', 'feeling': 'feline', 'Product': 'Purr-oduct', 'product': 'purr-oduct', 'mou': 'meow', 'br' : 'purr-', 'br ' : 'purr ', 'team': 'litter', 'followers': 'litter', 'people': 'cats', 'wrrr' : 'were', 'pir' : 'purr', 'kidding' : 'kitten' });
+    $(".vertical").html(text);
+    $("#cat").html();
+  })
+});
